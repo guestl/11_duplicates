5b6a1eae90af2c5022fc0f9c0333b499b2a2e42d
guestl <boris.nikulin@gmail.com>
2017-10-09 21:03:04 +0300

    First draft

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+This script receive a folder name as parameter, scan the folder and its sub-folders and print list of duplicated files in the  the folder.
+
+Duplicated files are files with the same file name and size.
+
+# How to run
+
+This script requires Python 3.5
+
+How yo run in Linux:
+
+```#!bash
+
+$ python duplicates.py -mf d:\test
+
+# possibly requires call of python3 executive instead of just python
+```
+
+Output example:
+
+```
+python duplicates.py -mf d:\test
+Scanning completed
+[['d:\\test\\111.docx', 'd:\\test\\444\\111.docx']]
+```
+
+There is no difference to run in Windows 
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,49 @@
+import logging
+import os.path
+import argparse
+import collections
+
+
+def get_file_dictionary(dir_name):
+    result_list = dict()
+
+    tree = os.walk(dir_name)
+
+    for disk, dirs, files in tree:
+        for file in files:
+            path = os.path.join(disk, file)
+            path = os.path.abspath(path)
+            result_list[path] = ''.join(
+                [file, ' - ', str(os.path.getsize(path))])
+
+    return result_list
+
 
 if __name__ == '__main__':
-    pass
+    # Look at command-line args
+    parser = argparse.ArgumentParser(description='This script scans\
+             for duplicated files in directories.')
+    parser.add_argument('--mf', '-mf', type=str, required=True,
+                        help='Main folder full path.')
+
+    args = parser.parse_args()
+
+    folder_name = args.mf
+
+    if os.path.isdir(folder_name):
+        print('Scanning..', end='\r')
+
+        file_list = get_file_dictionary(folder_name)
+
+        print('Scanning completed', end='\r')
+        print()
+    else:
+        logging.error("Folder '{}' must exists".format(folder_name))
+        exit()
+
+    dup_list = collections.defaultdict(list)
+
+    for key, value in file_list.items():
+        dup_list[value].append(key)
+
+    print([dup_items for dup_items in dup_list.values() if len(dup_items) > 1])
